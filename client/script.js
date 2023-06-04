98e9aebee39a08c36e4f77e9aded31b8f3dceca1
brainaxe1995 <sr.bd100@gmail.com>
2023-06-04 11:34:46 +0600

    asdf

--- client/script.js
+++ client/script.js
@@ -10,12 +10,10 @@ function loader(element) {
     element.textContent = 'Thinking'
 
     loadInterval = setInterval(() => {
-        // Update the text content of the loading indicator
-        element.textContent += '.';
-
-        // If the loading indicator has reached three dots, reset it
         if (element.textContent === 'Thinking....') {
             element.textContent = 'Thinking';
+        } else {
+            element.textContent += '.';
         }
     }, 300);
 }
@@ -33,15 +31,8 @@ function typeText(element, text) {
     }, 20)
 }
 
-// generate unique ID for each message div of bot
-// necessary for typing text effect for that specific reply
-// without unique ID, typing text will work on every element
 function generateUniqueId() {
-    const timestamp = Date.now();
-    const randomNumber = Math.random();
-    const hexadecimalString = randomNumber.toString(16);
-
-    return `id-${timestamp}-${hexadecimalString}`;
+    return `id-${Date.now()}-${Math.random().toString(16)}`;
 }
 
 function chatStripe(isAi, value, uniqueId) {
@@ -67,27 +58,16 @@ const handleSubmit = async (e) => {
 
     const data = new FormData(form)
 
-    // user's chatstripe
     chatContainer.innerHTML += chatStripe(false, data.get('prompt'))
-
-    // to clear the textarea input 
     form.reset()
 
-    // Remove classes from form and textarea
-    form.classList.remove('form-center')
-
-
-    // bot's chatstripe
     const uniqueId = generateUniqueId()
     chatContainer.innerHTML += chatStripe(true, " ", uniqueId)
 
-    // to focus scroll to the bottom 
     chatContainer.scrollTop = chatContainer.scrollHeight;
 
-    // specific message div 
     const messageDiv = document.getElementById(uniqueId)
 
-    // messageDiv.innerHTML = "..."
     loader(messageDiv)
 
     const response = await fetch('https://sb3ai.onrender.com', {
@@ -105,7 +85,7 @@ const handleSubmit = async (e) => {
 
     if (response.ok) {
         const data = await response.json();
-        const parsedData = data.bot.trim() // trims any trailing spaces/'\n' 
+        const parsedData = data.bot.trim()
 
         typeText(messageDiv, parsedData)
     } else {
--- server/server.js
+++ server/server.js
@@ -21,45 +21,42 @@ app.get('/', async (req, res) => {
   })
 })
 
-function containsAny(str, substrings) {
-  for (var i = 0; i !== substrings.length; i++) {
-     var substring = substrings[i];
-     if (str.indexOf(substring) !== -1) {
-       return true;
-     }
+function containsAny(source, targets) {
+  for (let target of targets) {
+    if (source.includes(target)) {
+      return true;
+    }
   }
-  return false; 
+  return false;
 }
 
 app.post('/', async (req, res) => {
   try {
-    const prompt = req.body.prompt;
-    const lowerCasePrompt = prompt.toLowerCase();
+    const prompt = req.body.prompt.toLowerCase();
+    let botResponse = "";
+
+    const nameQueries = ['your name?', 'what should i call you', 'what is your name?', 'whats is your name?'];
+    const identityQueries = ['are you a human or bot', 'who you are?', 'who are you?'];
+    const ageQueries = ['how old are you?', 'what is your age?'];
 
     const config = {
-      model: "text-davinci-003",
+      model: "text-curie-003",  // Smaller model, faster responses
       prompt: `I want you to act as an SB3 ai. ${prompt}`,
-      temperature: 0,
+      temperature: 0.7,  // Increase randomness of output
       max_tokens: 3000,
       top_p: 1,
       frequency_penalty: 0.5,
       presence_penalty: 0
     };
 
-    let botResponse = "";
-
-    const nameQueries = ['your name?', 'what should i call you', 'what is your name?', 'whats is your name?'];
-    const identityQueries = ['are you a humane or bot', 'who you are?', 'who are you?'];
-    const ageQueries = ['how old are you?', 'what is your age?'];
-
-    if (containsAny(lowerCasePrompt, nameQueries)) {
-      botResponse = 'You can call me SB3.ai. What can I help you with today?'
-    } else if (containsAny(lowerCasePrompt, identityQueries)) {
-      botResponse = 'As an SB3 AI, I am an artificial intelligence program designed to perform various tasks and provide information to users. ...'
-    } else if (containsAny(lowerCasePrompt, ageQueries)) {
-      botResponse = 'As an SB3 AI, I don\'t have an age in the traditional sense, as I am not a living being. ...'
+    if (containsAny(prompt, nameQueries)) {
+      botResponse = 'You can call me SB3.ai. What can I help you with today?';
+    } else if (containsAny(prompt, identityQueries)) {
+      botResponse = 'As an SB3 AI, I am an artificial intelligence program designed to perform various tasks and provide information to users. ...';
+    } else if (containsAny(prompt, ageQueries)) {
+      botResponse = 'As an SB3 AI, I don\'t have an age in the traditional sense, as I am not a living being. ...';
     } else if (prompt.trim() === '') {
-      botResponse = 'Sorry, I didn\'t understand your message. Could you please ask a question or provide some information that I can help you with?'
+      botResponse = 'Sorry, I didn\'t understand your message. Could you please ask a question or provide some information that I can help you with?';
     } else {
       const response = await openai.createCompletion(config);
       botResponse = response.data.choices[0].text;
